06d1d5775c73c06619ac420dd4a990d3a2154293
Paraleq <101002749+Paraleq@users.noreply.github.com>
2025-01-04 23:33:47 +0100

    Create Uruchom.bat

---
+++ Uruchom.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Uruchamianie skryptu TikTok Auto Liker & Commenter...
+
+:: Sprawdzanie, czy zainstalowano wymagane biblioteki
+echo Instalowanie bibliotek Selenium i WebDriver-Manager...
+pip install selenium webdriver-manager
+
+:: Uruchamianie skryptu Python
+echo Uruchamianie skryptu Python...
+python kodzik.py
+
+pause
